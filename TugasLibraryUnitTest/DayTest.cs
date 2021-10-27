75b48a141901d26694ed199d374bba098909571a
Darussalaam-Nur-Rasyidu <darussalaamnurrasyidu@gmail.com>
2021-10-27 16:49:18 +0700

    Add project files.

---
+++ TugasLibraryUnitTest/DayTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    class DayTest
+    {
+        Day day;
+        String[] nameDay;
+        [SetUp]
+        public void Init()
+        {
+            day = new Day();
+            nameDay = new string[7] { "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu", "Minggu" };
+        }
+        [Test]
+        public void NameOfDayTest()
+        {
+            for (int i = 0; i < 7; i++) Assert.AreEqual(nameDay[i], day.NameOfDay(i + 1));
+            Assert.AreEqual("", day.NameOfDay(8));
+        }
+    }
+}
---
+++ TugasLibraryUnitTest/UserTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    class UserTest
+    {
+        User user;
+        [SetUp]
+        public void Init()
+        {
+            user = new User();
+        }
+        [Test]
+        public void IsValidUserTest()
+        {
+            Assert.AreEqual(true, user.IsValidUser("admin", "admin"));
+            Assert.AreEqual(false, user.IsValidUser("nimda", "nimda"));
+        }
+    }
+}
